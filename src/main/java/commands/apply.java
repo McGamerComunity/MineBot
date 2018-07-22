e23c149a133b0b187a080f514fe554c6572cec6f
McGamerComunity <Tim24032003>
2018-07-22 13:33:07 +0200

    ADDET NEW COMMAND | VERSION 2.10

--- src/main/java/commands/apply.java
+++ src/main/java/commands/apply.java
@@ -15,7 +15,12 @@ public class apply implements commands {
     @Override
     public void action(String[] args, MessageReceivedEvent event) throws IOException, ParseException {
 
-        event.getTextChannel().sendMessage(MSGS.success().addField("Du kannst dich unter folgender E-Mail bewerben:","apply@ragefx.de" , true).build()).complete();
+        event.getTextChannel().sendMessage(MSGS.success()
+                .addField("Du kannst dich unter folgender E-Mail bewerben:","apply@ragefx.de" , false)
+                .addField("Vorraussetzungen:", "-Alter: mind. 14(<13 => informiere dich im support)\n-Geistige Reife\n-Vorkenntnisse Gern gesehen(mit serverangabe)", false)
+                .addField("Wie sollte meine Bewerbung\nstrukturiert sein:", "1.Persönliches\n2.Warum bin ich als Teammitglied bei ragefx.de geeignet\n3. Was sind meine Ziele bei ragefx.de\n4.Meine Stärken und Schwächen\n5.Schlusssatz(wenn du noch was loswerden willst)", false)
+
+                .build()).complete();
 
     }
 
